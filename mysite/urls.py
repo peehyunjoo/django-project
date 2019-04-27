6dbf4bd632c54149d63fa64ae6ec230385a04732
peehyunjoo <peehyunjoo@gmail.com>
2019-04-27 13:52:32 +0900

    예제 확장
    예제 확장

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # shkim
     path('polls/', include('polls.urls')),
+    path('books/', include('books.urls')),
 ]
